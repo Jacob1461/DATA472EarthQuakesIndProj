ae974e0abd17bcb4e62e00aec3468ff34a2d60f6
Jacob Clinton <jacoblclinton@gmail.com>
2024-05-08 21:12:09 +1200

    Updated the create_database file to reflect new database design. Added new table for grouping

--- src/database/create_database.jl
+++ src/database/create_database.jl
@@ -4,36 +4,41 @@ function create_database()
     db = SQLite.DB("Earthquakes.db") # "Earthquakes.db" add to argument to make file based rather than in memory
 
     SQLite.execute(db, """
-    CREATE TABLE IF NOT EXISTS earthquake_table (
-        publicID TEXT PRIMARY KEY,
-        country TEXT,
-        time TEXT)""")
-
-    SQLite.execute(db, """
-    CREATE TABLE IF NOT EXISTS earthquake_details (
-        details_id INTEGER PRIMARY KEY,
-        publicID TEXT UNIQUE,
-        locality TEXT,
-        magnitude REAL,
-        magtype TEXT,
-        mmi REAL,
-        latitude REAL,
-        depth REAL,
-        longitude REAL,
-        FOREIGN KEY(publicID) REFERENCES earthquake_table(publicID))""")
-    SQLite.execute(db, "DROP TABLE IF EXISTS temp_insert_data")
-    SQLite.execute(db, """
-    CREATE TABLE temp_insert_data (
-        publicID TEXT PRIMARY KEY,
+    CREATE TABLE IF NOT EXISTS earthquakes_table (
+        earthquakeID TEXT PRIMARY KEY,
         country TEXT,
         time TEXT,
         magnitude REAL,
-        magtype TEXT, 
-        mmi REAL,
-        locality TEXT,
+        locality TEXT, 
         depth REAL,
+        mmi REAL,
         latitude REAL,
-        longitude REAL)""")
+        longitude REAL,
+        source TEXT,
+        publicID TEXT)""")
+
+    SQLite.execute(db, "DROP TABLE IF EXISTS temp_insert_data")
+    SQLite.execute(db, """
+    CREATE TABLE temp_insert_data (
+        earthquakeID TEXT PRIMARY KEY,
+        country TEXT,
+        time TEXT,
+        magnitude REAL,
+        locality TEXT, 
+        depth REAL,
+        mmi REAL,
+        latitude REAL,
+        longitude REAL,
+        source TEXT,
+        publicID TEXT)""")
+
+    SQLite.execute(db, """
+        CREATE TABLE IF NOT EXISTS grouping_table (
+        earthquakeID TEXT,
+        source TEXT,
+        groupID INTEGER,
+        PRIMARY KEY (earthquakeID, groupID),
+        FOREIGN KEY (earthquakeID) REFERENCES earthquakes_table(earthquakeID))""")
 
     #println(SQLite.tables(db))
     return db
