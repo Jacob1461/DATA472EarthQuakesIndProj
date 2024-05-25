dc6a2fc7c9e1c2ae473229214bcb20847d02ce92
Jacob1461 <35879589+Jacob1461@users.noreply.github.com>
2024-05-25 21:40:43 +1200

    Updated script for inserting values into table

--- src/database/insert_into_db.jl
+++ src/database/insert_into_db.jl
@@ -2,25 +2,21 @@ using MySQL, DBInterface
 using DataFrames
 using Dates
 
-function insert_into_db(db, data_to_insert::DataFrame)
+function insert_into_db(db::MySQL.Connection, data_to_insert::DataFrame)
     data_to_insert.time = string.(data_to_insert.time, "yyyy-mm-ddTHH:MM:SS.sssZ")
     unique!(data_to_insert, :earthquakeID)
-    DBInterface.execute(db, "CREATE TEMPORARY TABLE temp_insert_data LIKE earthquakes_table")
-    DBInterface.load!(data_to_insert, db, "temp_insert_data")
-
-    DBInterface.execute(db, "START TRANSACTION")
-    DBInterface.execute(db, """
-    DELETE FROM temp_insert_data
-    WHERE earthquakeID IN (SELECT earthquakeID FROM earthquakes_table)
-    """)
-    DBInterface.execute(db, """
+    sql = """
     INSERT INTO earthquakes_table (earthquakeID, country, time, magnitude, locality, depth, mmi, latitude, longitude, source, publicID)
-    SELECT earthquakeID, country, time, magnitude, locality, depth, mmi, latitude, longitude, source, publicID FROM temp_insert_data
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     ON DUPLICATE KEY UPDATE
     country = VALUES(country), time = VALUES(time), magnitude = VALUES(magnitude), locality = VALUES(locality),
     depth = VALUES(depth), mmi = VALUES(mmi), latitude = VALUES(latitude), longitude = VALUES(longitude),
     source = VALUES(source), publicID = VALUES(publicID)
-    """)
-    DBInterface.execute(db, "TRUNCATE TABLE temp_insert_data")
-    DBInterface.execute(db, "COMMIT")
+    """
+    MySQL.execute(db, "START TRANSACTION")
+    
+    for row in eachrow(data_to_insert)
+        MySQL.execute(db, sql, row.earthquakeID, row.country, row.time, row.magnitude, row.locality, row.depth, row.mmi, row.latitude, row.longitude, row.source, row.publicID)
+    end
+    MySQL.execute(db, "COMMIT")
 end
