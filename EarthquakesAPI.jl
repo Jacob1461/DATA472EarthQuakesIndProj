b6c66f756f0ed422ed0a6263e79f8f2952a50fc9
Jacob1461 <35879589+Jacob1461@users.noreply.github.com>
2024-05-29 19:37:17 +1200

    Updated API functionality and added basic error catching, updated dockerfile

--- EarthquakesAPI.jl
+++ EarthquakesAPI.jl
@@ -9,13 +9,19 @@ include("src/API/database_connection.jl")
 include("src/API/query_to_json.jl")
 include("src/API/QueryDB.jl")
 
-@get "/data" function(req::HTTP.Request, limit::Int=100)
-    conn = database_connection()
-    dataf = query_database(conn, limit)
-    res = create_geojson(dataf)
-    return res
+@get "/data" function(req::HTTP.Request, limit=100)
+    try 
+        @assert isa(limit, Int)
+        @assert limit > 0
+        conn = database_connection()
+        dataf = query_database(conn, limit)
+        res = create_geojson(dataf)
+        DBInterface.close!(conn)
+        return res
+    catch e
+        return HTTP.Response(500, JSON.json(Dict("error" => "Internal server error", "details" => string(e))))
+    end
 end
 
 
 serve(host="0.0.0.0", port=8080)
-
--- setup.jl
+++ setup.jl
@@ -1,29 +1,18 @@
 using Pkg
 
-# Activate the environment located in the same directory as this file
 Pkg.activate(@__DIR__)
-
-# Ensure that all dependencies specified in the Project.toml and Manifest.toml files are installed
 Pkg.instantiate()
-
-# Update all installed packages
 Pkg.update()
-
-# List of packages to add (if not already included in Project.toml)
 packages = ["HTTP", "JSON", "DataFrames", "Printf", "DBInterface", "AWS", "AWSS3", "Oxygen", "JSON3", "MySQL", "DataStructures"]
 
-# Add each package to the environment
 for package in packages
     Pkg.add(package)
 end
 
-# Resolve any dependencies
 Pkg.resolve()
 
-# Precompile all installed packages
 Pkg.precompile()
 
-# Now it's safe to import the packages
 using HTTP
 using JSON
 using DataFrames
