eb1ae12fa1350939414d56fbd0520e02d03de8e9
Jacob1461 <35879589+Jacob1461@users.noreply.github.com>
2024-05-31 22:19:21 +1200

    Added bash script to make sure a container is running and is reset daily

---
+++ EC2/earthquakes_updater_runner.sh
@@ -0,0 +1,17 @@
+LOG_DIR="/home/ubuntu/"
+LOG_FILE_PATTERN="$LOG_DIR/logfile-$(date +'%Y-%m-%d').log"
+
+run_container() {
+  sudo docker run --env-file .env -p 8080:8080 earthquakesupdater > $LOG_FILE_PATTERN 2>&1
+}
+
+while true; do
+  sudo docker stop earthquake_updater
+  sudo docker rm earthquake_updater
+  run_container
+
+  find $LOG_DIR -name 'logfile-*.log' -mtime +7 -exec rm {} \; #Delete logs older than a week
+
+
+  sleep 86400 # wait 24 hrs
+done
